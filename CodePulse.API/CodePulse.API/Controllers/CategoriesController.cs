d4b692c7759dcf50b842d80a8f6133ee92b5ca93
CHUKWUDI INNOCENT <125125776+Innochu@users.noreply.github.com>
2023-11-06 15:54:15 +0100

    injected db context into our controller and craeted a post controller but would modify once i create my repo pattern and services

--- CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
+++ CodePulse.API/CodePulse.API/Controllers/CategoriesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CodePulse.Application.DTO.RequestDTO;
+using CodePulse.Domain.Models;
+using CodePulse.Infrastructure;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodePulse.API.Controllers
@@ -8,6 +11,27 @@ namespace CodePulse.API.Controllers
     [ApiController]
     public class CategoriesController : ControllerBase
     {
+        private readonly ApplicationDbContext _applicationDbContext;
 
+        public CategoriesController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        //https: localhost: xxxx/api/createCategories
+        [HttpPost]
+        public async Task<IActionResult> CreatCategory(PostCategoryRequestDTO postCategoryRequestDTO)
+        {
+            // map DTO to Domain Model
+
+            var cat = new Category
+            {
+                Name = postCategoryRequestDTO.Name,
+                Urlhandle = postCategoryRequestDTO.Urlhandle,
+            };
+
+            await _applicationDbContext.SaveChangesAsync();
+            return Ok(cat);
+        }
     }
 }
